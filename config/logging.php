7a066d056b9cb52fcb1f3fcbce0dc1d67b91b283
AlnadhariMustafa <saeed20@graduate.utm.my>
2022-12-31 18:57:10 +0800

    commit

--- config/logging.php
+++ config/logging.php
@@ -63,7 +63,20 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
-//
+        'daily' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+        ],
+
+        'slack' => [
+            'driver' => 'slack',
+            'url' => env('LOG_SLACK_WEBHOOK_URL'),
+            'username' => 'Laravel Log',
+            'emoji' => ':boom:',
+            'level' => env('LOG_LEVEL', 'critical'),
+        ],
 
         'papertrail' => [
             'driver' => 'monolog',
