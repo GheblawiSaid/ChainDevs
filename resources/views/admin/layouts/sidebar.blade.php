01b606422db73de9a3b53b705e6886d3fef303a7
AlnadhariMustafa <saeed20@graduate.utm.my>
2023-01-13 20:46:27 +0800

    COMMIT

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,37 +1,22 @@
-  <!-- Main Sidebar Container -->
+<?php
+
+  $role=Auth::user()->role_id;
+
+?>
+
+
+<!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="/dashboard" class="brand-link">
       <span class="brand-text font-weight-light">Rides Booking</span>
     </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Administrator</a>
-        </div>
-      </div>
-
       <!-- Sidebar Menu -->
       <nav class="mt-2">
+        @if($role == 1)
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-          </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tree"></i>
@@ -68,6 +53,12 @@
                 <a href="/addChat" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Chat</p>
+                </a>
+              </li>
+               <li class="nav-item">
+                <a href="/addGroupChat" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Group Chat</p>
                 </a>
               </li>
             </ul>
@@ -112,9 +103,94 @@
               </li>
             </ul>
           </li>
-        </ul>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tree"></i>
+              <p>
+               Profile
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="/showProfile" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Show</p>
+                </a>
+              </li>
+            </ul>
           </li>
         </ul>
+        @else
+     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tree"></i>
+              <p>
+                Rides
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="/addRide" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Add Ride</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/showAllRidesOfLoggedInUser" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Show Rides</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tree"></i>
+              <p>
+                Chats
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="/addChat" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Chat</p>
+                </a>
+              </li>
+               <li class="nav-item">
+                <a href="/addGroupChat" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Group Chat</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tree"></i>
+              <p>
+               Profile
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="/showProfile" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Show</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        @endif
       </nav>
       <!-- /.sidebar-menu -->
     </div>
